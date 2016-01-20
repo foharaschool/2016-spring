980ddb0985d35b463a4c7347f53108bb912c2352
foharaschool <fohara@madisoncollege.edu>
2016-01-19 20:38:34 -0600

    introJava intro programs

---
+++ introJava/projects/src/java111/project1/labs/LabOneAssignment.java
@@ -0,0 +1,22 @@
+/**
+ * This is the first class in the first lab of the first semester of Java!
+ * 
+ * @author fohara
+ * 
+ */
+ public class LabOneAssignment
+ {
+     
+    /**
+    * This is the main method for this class
+    *
+    * @param args
+    */
+    public static void main (String[] args)
+    {
+        int number = 25;
+        String name = "Fred";
+        System.out.println(number);
+        System.out.println(name);
+    }
+ }
