b49728c8d6b9aaf23e32f2ee37984c7f977078ab
foharaschool <fohara@madisoncollege.edu>
2016-04-19 18:52:38 -0500

    Java Proj5 lab1

---
+++ introJava/projects/src/java111/project5/labs/Lab51.java
@@ -0,0 +1,22 @@
+package java111.project5.labs;
+ 
+import java.util.*;
+ 
+/**
+ *  Lab 5-1
+ *
+ *@author    eknapp
+ */
+public class Lab51 extends java.lang.Object {
+ 
+    /**
+     *  Main processing method for the Lab51 object
+     */
+    public void run() {
+ 
+        System.out.println("[java111.project5.labs.Lab51 > run( )]"
+                 + (new Date()).toLocaleString());
+ 
+    }
+ 
+}
---
+++ introJava/projects/src/java111/project5/labs/Lab51TestDrive.java
@@ -0,0 +1,23 @@
+package java111.project5.labs;
+ 
+/**
+ *  Lab 5-1 with multiple classes
+ *
+ *@author    eknapp
+ */
+public class Lab51TestDrive extends java.lang.Object {
+ 
+    /**
+     *  The main program for the Lab51TestDrive class
+     *
+     *@param  args  The command line arguments
+     */
+    public static void main(String[] args) {
+ 
+        Lab51 lab = new Lab51();
+         
+        lab.run();
+         
+    }
+ 
+}
